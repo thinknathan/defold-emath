0aa88ace294514c038afab80d601bb4bc0f9f365
Nathan Bolton <nathan@nathanbolton.ca>
2023-06-01 17:18:33 -0700

    Update types

--- @types/defold-emath.d.ts
+++ @types/defold-emath.d.ts
@@ -2,18 +2,19 @@
 
 /**
  * Extended math native extension
+ * @noResolution
  */
 declare namespace emath {
 	/**
 	 * Ensures the `input` number is not less than `lower`, or larger than `upper`
-	 * @param input 
-	 * @param lower 
-	 * @param upper 
+	 * @param input
+	 * @param lower
+	 * @param upper
 	 */
 	export function clamp(input: number, lower: number, upper: number): number;
 	/**
 	 * Rounds the `input` number to the nearest integer
-	 * @param input 
+	 * @param input
 	 */
 	export function round(input: number): number;
 }
